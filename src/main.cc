b8a95e45a01777b6a7e47fed47c8d079e27724dc
Jacques Lucke <mail@jlucke.com>
2020-04-05 12:26:12 +0200

    first window

--- src/main.cc
+++ src/main.cc
@@ -2,12 +2,38 @@
 
 #include "bas/vector.h"
 
+#include "glad/glad.h"
+
+#include "GLFW/glfw3.h"
+
 using bas::Vector;
 
 int main()
 {
-    Vector<int> vec = {1, 2, 3, 4, 5};
-    vec.as_ref().print_as_lines("vec");
-    std::cout << "Hello World\n";
+    if (!glfwInit()) {
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(
+        640, 480, "My Title", nullptr, nullptr);
+    if (window == nullptr) {
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
